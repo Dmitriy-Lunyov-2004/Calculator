57aa450d669c0136e7d91ee0d9db7ef62b657df6
Dmitriy_Lunyov_2004 <Dmitriy_Lunyov_2004@mail.ru>
2022-07-18 19:22:34 +0300

    - add backspace button

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-function on_click_num(event)
-{
+function get_target() {
     let enviroment = document.body
     .getElementsByClassName('calculator-interface')[0];
 
     let target = enviroment.getElementsByClassName('calculator-input')[0];
 
-    let source = event.target;
+    return target
+}
 
+function on_click_num(event)
+{
+    let target = get_target()
     target.innerHTML += event.target.innerHTML;
+}
+
+function backspace(event) {
+    let target = get_target()
+    let s = target.innerHTML;
+    target.innerHTML = s.slice(0, s.length - 1)
 }
